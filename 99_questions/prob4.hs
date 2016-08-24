ea81d1bdd5d8cd557e3af797c2d957ec66c14962
Ashutosh Tripathi <azbycs@gmail.com>
2016-08-24 23:01:26 +0530

    Problem 4. Add other variants

--- 99_questions/prob4.hs
+++ 99_questions/prob4.hs
@@ -7,3 +7,9 @@ myLength xs = sum [1 | _ <- xs]
 myLength' :: (Integral b) => [a] -> b
 myLength' [] = 0
 myLength' (_:xs) = 1 + (myLength' xs)
+
+myLength'' :: (Integral b) => [a] -> b
+myLength'' = foldl (\acc x -> acc + 1) 0 
+
+myLength''' :: (Integral b) => [a] -> b
+myLength''' = sum . (map (\_ -> 1))
