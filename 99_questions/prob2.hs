cda0246a80245251fc85e465817677a4bcf1d3ae
Ashutosh Tripathi <azbycs@gmail.com>
2016-08-24 22:17:51 +0530

    Problem 2. Add another variant

--- 99_questions/prob2.hs
+++ 99_questions/prob2.hs
@@ -6,3 +6,9 @@ find2ndLast [] = error "Empty List"
 find2ndLast [x] = error "Empty List"
 find2ndLast [x,y] = x
 find2ndLast (x:xs) = find2ndLast xs
+
+
+find2ndLast' :: [a] -> a
+find2ndLast' [] = error "Empty list"
+find2ndLast' [x] = error "Empty list"
+find2ndLast' lst = (!!1) . reverse $ lst
